3aa76bcc6b57be8d79ca94458ab4b50bd56bb2bd
Valeriy Emelyanov <emelva@rambler.ru>
2022-04-20 10:09:19 +0300

    Stage #2

--- Steganography and Cryptography/task/src/cryptography/Main.kt
+++ Steganography and Cryptography/task/src/cryptography/Main.kt
@@ -10,11 +10,49 @@ fun main() {
         val task = readLine()!!
         when (task) {
             "exit" -> break
-            "hide" -> println("Hiding message in image.")
+            "hide" -> hideMessage()
             "show" -> println("Obtaining message from image.")
             else -> println("Wrong task: $task")
         }
     }
 
     println("Bye!")
+}
+
+const val LEAST_ONE_BITS = 65793
+
+fun hideMessage() {
+    println("Input image file:")
+    val inputFileName = readLine()!!
+    println("Output image file:")
+    val outputFileName = readLine()!!
+
+    val image: BufferedImage
+    try {
+        val inputFile = File(inputFileName)
+        image = ImageIO.read(inputFile)
+    } catch (e: Exception) {
+        println("Can't read input file!")
+        return
+    }
+
+    println("Input Image: $inputFileName")
+    println("Output Image: $outputFileName")
+
+    for (x in 0 until image.width) {
+        for (y in 0 until image.height) {
+            val rgb = image.getRGB(x, y)
+            image.setRGB(x, y, rgb or LEAST_ONE_BITS)
+        }
+    }
+
+    try {
+        val outputFile = File(outputFileName)
+        ImageIO.write(image, "png", outputFile)
+    } catch (e: Exception) {
+        println("Can't write output file!")
+        return
+    }
+
+    println("Image $outputFileName is saved.")
 }
